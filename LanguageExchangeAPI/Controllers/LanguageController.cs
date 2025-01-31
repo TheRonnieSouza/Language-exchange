8fd7fd6c44ccb807e78497ec215d87195948daf2
TheRonnieSouza <ronniesouza.contato@gmail.com>
2025-01-31 07:53:07 -0300

    Feat - Controller language

---
+++ LanguageExchangeAPI/Controllers/LanguageController.cs
@@ -0,0 +1,41 @@
+﻿using LanguageExchange.Application.Models.LanguagesModels;
+using LanguageExchange.Application.Services.Language;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageExchangeAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LanguageController : ControllerBase
+    {
+        private readonly ILanguageService _languageService;
+        public LanguageController(ILanguageService languageService)
+        {
+            _languageService = languageService;
+        }
+
+        [HttpPost] 
+        public async Task<IActionResult> CreateLanguage(CreateLanguageInputModel language)
+        {
+            var result = await _languageService.AddLanguage(language);
+
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllLanguages()
+        {
+            var result = await _languageService.GetAllLanguages();
+
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLanguage(int id)
+        {
+            var result = await _languageService.DeleteLanguage(id);
+
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
+        }
+    }
+}
